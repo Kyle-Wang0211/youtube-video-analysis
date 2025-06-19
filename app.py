8e79add1d181cebd9b757e3b1160379dfb8480bd
Yusheng-Qian <yq2339@nyu.edu>
2025-06-19 08:55:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+import shap
+import mlflow
+from dagshub import DAGsHubLogger
 
 st.set_page_config(
     page_title="📊 YouTube Video Analysis APP",
